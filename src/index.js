c792b39fe482762f515078685c4ab020564470c6
wynhsu <wynhsu@uw.edu>
2018-07-14 15:00:36 -0700

    run deploy

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App.js';
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
 ReactDOM.render(<App />, document.getElementById("index"));
